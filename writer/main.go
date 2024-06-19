149e9ad12a35fb7ec4fd4fcf8ae4edd428315323
kkbase <kkbase@163.com>
2024-06-19 16:33:18 +0800

    fast write ok

--- writer/main.go
+++ writer/main.go
@@ -1,24 +1,22 @@
 package main
 
+// #cgo CFLAGS: -I../plugin
+// #include "dylib.h"
+// #include "write_plugin.h"
 import "C"
 import (
 	"bufio"
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
-	"sync/atomic"
 )
 
-// #cgo CFLAGS: -I../plugin
-// #include "dylib.h"
-// #include "write_plugin.h"
-import "C"
-
-const CacheSize = 64
+const CacheSize = 128
 
 type AnalogSection struct {
 	Time int64
@@ -38,10 +36,169 @@ type StaticDigitalSection struct {
 	Data []C.StaticDigital
 }
 
+// ParseAnalogRecord 解析CSV行
+func ParseAnalogRecord(record []string) (int64, C.Analog, error) {
+	analog := C.Analog{}
+
+	// 去除首行
+	if record[0] == "TIME" {
+		return -1, analog, errors.New("continue HEAD")
+	}
+
+	// 去除尾行
+	if len(record) != 11 {
+		return -1, analog, errors.New("continue TAIL")
+	}
+
+	// 解析行
+	// TIME,P_NUM,AV,AVR,Q,BF,FQ,FAI,MS,TEW,CST
+	time, err := strconv.ParseInt(record[0], 10, 64)
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse time error", record[0]))
+	}
+
+	pNum, err := strconv.ParseInt(record[1], 10, 32)
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse pNum error", record[1]))
+	}
+
+	av, err := strconv.ParseFloat(record[2], 64)
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse av error", record[2]))
+	}
+	avr, err := strconv.ParseFloat(record[3], 64)
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse avr error", record[3]))
+	}
+
+	q, err := strconv.ParseBool(record[4])
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse q error", record[4]))
+	}
+
+	bf, err := strconv.ParseBool(record[5])
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse bf error", record[5]))
+	}
+
+	qf, err := strconv.ParseBool(record[6])
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse qf error", record[6]))
+	}
+
+	fai, err := strconv.ParseFloat(record[7], 32)
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse fai error", record[7]))
+	}
+
+	ms, err := strconv.ParseBool(record[8])
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse ms error", record[8]))
+	}
+
+	if len(record[9]) != 1 {
+		return -1, analog, errors.New(fmt.Sprintln("parse ms error", record[9]))
+	}
+	tew := record[9][0]
+
+	cst, err := strconv.ParseInt(record[10], 10, 32)
+	if err != nil {
+		return -1, analog, errors.New(fmt.Sprintln("parse cst error", record[10]))
+	}
+
+	analog.p_num = C.int32_t(pNum)
+	analog.av = C.float(av)
+	analog.avr = C.float(avr)
+	analog.q = C.bool(q)
+	analog.bf = C.bool(bf)
+	analog.qf = C.bool(qf)
+	analog.fai = C.float(fai)
+	analog.ms = C.bool(ms)
+	analog.tew = C.char(tew)
+	analog.cst = C.uint16_t(cst)
+
+	return time, analog, nil
+}
+
+// ParseDigitalRecord 解析CSV行
+func ParseDigitalRecord(record []string) (int64, C.Digital, error) {
+	digital := C.Digital{}
+
+	// 去除首行
+	if record[0] == "TIME" {
+		return -1, digital, errors.New("continue HEAD")
+	}
+
+	// 去除尾行
+	if len(record) != 11 {
+		return -1, digital, errors.New("continue TAIL")
+	}
+
+	time, err := strconv.ParseInt(record[0], 10, 64)
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse time error", record[0]))
+	}
+
+	pNum, err := strconv.ParseInt(record[1], 10, 32)
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse pNum error", record[1]))
+	}
+	dv, err := strconv.ParseBool(record[2])
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse dv error", record[2]))
+	}
+	dvr, err := strconv.ParseBool(record[3])
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse dvr error", record[3]))
+	}
+	q, err := strconv.ParseBool(record[4])
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse q error", record[4]))
+	}
+	bf, err := strconv.ParseBool(record[5])
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse bf error", record[5]))
+	}
+	bq, err := strconv.ParseBool(record[6])
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse bq error", record[6]))
+	}
+	fai, err := strconv.ParseBool(record[7])
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse fai error", record[7]))
+	}
+	ms, err := strconv.ParseBool(record[8])
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse ms error", record[8]))
+	}
+	if len(record[9]) != 1 {
+		return -1, digital, errors.New(fmt.Sprintln("parse ms error", record[9]))
+	}
+	tew := record[9][0]
+	cst, err := strconv.ParseInt(record[10], 10, 32)
+	if err != nil {
+		return -1, digital, errors.New(fmt.Sprintln("parse cst error", record[10]))
+	}
+
+	// 拼接数据, 并且添加到dataList
+	digital.p_num = C.int32_t(pNum)
+	digital.dv = C.bool(dv)
+	digital.dvr = C.bool(dvr)
+	digital.q = C.bool(q)
+	digital.bf = C.bool(bf)
+	digital.bq = C.bool(bq)
+	digital.fai = C.bool(fai)
+	digital.ms = C.bool(ms)
+	digital.tew = C.char(tew)
+	digital.cst = C.uint16_t(cst)
+
+	return time, digital, nil
+}
+
 // ReadAnalogCsv 读取CSV文件, 将其转换成 C.Analog 结构后发送到缓存队列
-func ReadAnalogCsv(filepath string, ch chan AnalogSection, wg *sync.WaitGroup, closeCh chan bool) {
+func ReadAnalogCsv(wg *sync.WaitGroup, closeCh chan struct{}, filepath string, ch chan AnalogSection) {
 	defer wg.Done()
-	fmt.Println("filepath: ", filepath)
+
 	// 打开文件
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -50,92 +207,32 @@ func ReadAnalogCsv(filepath string, ch chan AnalogSection, wg *sync.WaitGroup, c
 	defer func() { _ = file.Close() }()
 
 	// CSV读取器
-	reader := csv.NewReader(bufio.NewReader(file))
+	reader := csv.NewReader(NewCRFilterReader(bufio.NewReader(file)))
 
+	// 按行读取
 	dataList := make([]C.Analog, 0)
 	timeFlag := int64(-1)
-	// 按行读取
 	for {
 		// 读取一行, 判断是否为EOF
 		record, err := reader.Read()
 		if err != nil {
 			if err.Error() == "EOF" {
 				if len(dataList) != 0 {
-					fmt.Println("send!: ", len(dataList), timeFlag)
+					fmt.Println("send: ", filepath, len(dataList))
 					ch <- AnalogSection{Time: timeFlag, Data: dataList}
 				}
-				closeCh <- true
+				closeCh <- struct{}{}
 				break
 			}
 			log.Printf("Error reading record: %s", err)
 			continue
 		}
 
-		// 去除首行
-		if record[0] == "TIME" {
-			continue
-		}
-		// 去除尾行
-		if len(record) != 11 {
-			continue
-		}
-
-		// 解析行
-		// TIME,P_NUM,AV,AVR,Q,BF,FQ,FAI,MS,TEW,CST
-		time, err := strconv.ParseInt(record[0], 10, 64)
+		time, analog, err := ParseAnalogRecord(record)
 		if err != nil {
-			log.Println("parse time error", record[0])
-			continue
-		}
-		pNum, err := strconv.ParseInt(record[1], 10, 32)
-		if err != nil {
-			log.Println("parse pNum error", record[1])
-			continue
-		}
-		av, err := strconv.ParseFloat(record[2], 64)
-		if err != nil {
-			log.Println("parse av error", record[2])
-			continue
-		}
-		avr, err := strconv.ParseFloat(record[3], 64)
-		if err != nil {
-			log.Println("parse avr error", record[3])
-			continue
-		}
-		q, err := strconv.ParseBool(record[4])
-		if err != nil {
-			log.Println("parse q error", record[4])
-			continue
-		}
-		bf, err := strconv.ParseBool(record[5])
-		if err != nil {
-			log.Println("parse bf error", record[5])
-			continue
-		}
-		qf, err := strconv.ParseBool(record[6])
-		if err != nil {
-			log.Println("parse qf error", record[6])
-			continue
-		}
-		fai, err := strconv.ParseFloat(record[7], 32)
-		if err != nil {
-			log.Println("parse fai error", record[7])
-			continue
-		}
-		ms, err := strconv.ParseBool(record[8])
-		if err != nil {
-			log.Println("parse ms error", record[8])
-			continue
-		}
-		if len(record[9]) != 1 {
-			log.Println("parse tew error", record[9])
-			continue
-		}
-		tew := record[9][0]
-
-		cst, err := strconv.ParseInt(strings.TrimSuffix(record[10], "\r"), 10, 32)
-		if err != nil {
-			log.Println("parse cst error", record[10])
+			if !strings.Contains(err.Error(), "continue HEAD") {
+				log.Printf("Error parsing record: %s", err)
+			}
 			continue
 		}
 
@@ -144,42 +241,23 @@ func ReadAnalogCsv(filepath string, ch chan AnalogSection, wg *sync.WaitGroup, c
 			timeFlag = time
 		}
 
-		if len(dataList)%1000 != 0 {
-			fmt.Println("len: ", len(dataList))
-		}
-
 		// 如果出现的时间戳, 则更新timeFlag, 发送数据, 并且清空dataList
 		if timeFlag != time {
-			if len(dataList) != 0 {
-				fmt.Println("send!: ", len(dataList), timeFlag)
-				ch <- AnalogSection{Time: timeFlag, Data: dataList}
-			}
+			fmt.Println("send: ", filepath, len(dataList), "t1: ", timeFlag, "t2", time)
+			ch <- AnalogSection{Time: timeFlag, Data: dataList}
 			timeFlag = time
-
 			dataList = make([]C.Analog, 0)
 		}
 
-		// 拼接数据, 并且添加到dataList
-		analog := C.Analog{}
-		analog.p_num = C.int32_t(pNum)
-		analog.av = C.float(av)
-		analog.avr = C.float(avr)
-		analog.q = C.bool(q)
-		analog.bf = C.bool(bf)
-		analog.qf = C.bool(qf)
-		analog.fai = C.float(fai)
-		analog.ms = C.bool(ms)
-		analog.tew = C.char(tew)
-		analog.cst = C.uint16_t(cst)
-
+		// dataList 插入
 		dataList = append(dataList, analog)
 	}
 
-	close(ch)
+	// close(ch)
 }
 
 // ReadDigitalCsv 读取CSV文件, 将其转换成 C.Digital 结构后发送到缓存队列
-func ReadDigitalCsv(filepath string, ch chan DigitalSection, wg *sync.WaitGroup, closeCh chan bool) {
+func ReadDigitalCsv(wg *sync.WaitGroup, closeCh chan struct{}, filepath string, ch chan DigitalSection) {
 	defer wg.Done()
 
 	// 打开文件
@@ -190,7 +268,7 @@ func ReadDigitalCsv(filepath string, ch chan DigitalSection, wg *sync.WaitGroup,
 	defer func() { _ = file.Close() }()
 
 	// CSV读取器
-	reader := csv.NewReader(bufio.NewReader(file))
+	reader := csv.NewReader(NewCRFilterReader(bufio.NewReader(file)))
 
 	// 按行读取
 	dataList := make([]C.Digital, 0)
@@ -201,77 +279,21 @@ func ReadDigitalCsv(filepath string, ch chan DigitalSection, wg *sync.WaitGroup,
 		if err != nil {
 			if err.Error() == "EOF" {
 				if len(dataList) != 0 {
+					fmt.Println("send: ", filepath, len(dataList))
 					ch <- DigitalSection{Time: timeFlag, Data: dataList}
 				}
-				closeCh <- true
+				closeCh <- struct{}{}
 				break
 			}
 			log.Printf("Error reading record: %s", err)
 			continue
 		}
 
-		// 去除首行
-		if record[0] == "TIME" {
-			continue
-		}
-		// 去除尾行
-		if len(record) != 11 {
-			continue
-		}
-
-		time, err := strconv.ParseInt(record[0], 10, 64)
+		time, digital, err := ParseDigitalRecord(record)
 		if err != nil {
-			log.Println("parse time error", record[0])
-			continue
-		}
-		pNum, err := strconv.ParseInt(record[1], 10, 32)
-		if err != nil {
-			log.Println("parse pNum error", record[1])
-			continue
-		}
-		dv, err := strconv.ParseBool(record[2])
-		if err != nil {
-			log.Println("parse dv error", record[2])
-			continue
-		}
-		dvr, err := strconv.ParseBool(record[3])
-		if err != nil {
-			log.Println("parse dvr error", record[3])
-			continue
-		}
-		q, err := strconv.ParseBool(record[4])
-		if err != nil {
-			log.Println("parse q error", record[4])
-			continue
-		}
-		bf, err := strconv.ParseBool(record[5])
-		if err != nil {
-			log.Println("parse bf error", record[5])
-			continue
-		}
-		bq, err := strconv.ParseBool(record[6])
-		if err != nil {
-			log.Println("parse bq error", record[6])
-			continue
-		}
-		fai, err := strconv.ParseBool(record[7])
-		if err != nil {
-			log.Println("parse fai error", record[7])
-			continue
-		}
-		ms, err := strconv.ParseBool(record[8])
-		if err != nil {
-			log.Println("parse ms error", record[8])
-			continue
-		}
-		if len(record[9]) != 1 {
-			log.Println("parse tew error", record[9])
-			continue
-		}
-		tew := record[9][0]
-		cst, err := strconv.ParseInt(strings.TrimSuffix(record[10], "\r"), 10, 32)
-		if err != nil {
-			log.Println("parse cst error", record[10])
+			if !strings.Contains(err.Error(), "continue HEAD") {
+				log.Printf("Error parsing record: %s", err)
+			}
 			continue
 		}
 
@@ -280,105 +302,75 @@ func ReadDigitalCsv(filepath string, ch chan DigitalSection, wg *sync.WaitGroup,
 			timeFlag = time
 		}
 
-		if len(dataList)%1000 == 0 {
-			fmt.Println("len: ", len(dataList))
-		}
-
 		// 如果出现的时间戳, 则更新timeFlag, 发送数据, 并且清空dataList
 		if timeFlag != time {
 			if len(dataList) != 0 {
+				fmt.Println("send: ", filepath, len(dataList), "t1: ", timeFlag, "t2", time)
 				ch <- DigitalSection{Time: timeFlag, Data: dataList}
 			}
 			timeFlag = time
 			dataList = make([]C.Digital, 0)
 		}
 
-		// 拼接数据, 并且添加到dataList
-		digital := C.Digital{}
-		digital.p_num = C.int32_t(pNum)
-		digital.dv = C.bool(dv)
-		digital.dvr = C.bool(dvr)
-		digital.q = C.bool(q)
-		digital.bf = C.bool(bf)
-		digital.bq = C.bool(bq)
-		digital.fai = C.bool(fai)
-		digital.ms = C.bool(ms)
-		digital.tew = C.char(tew)
-		digital.cst = C.uint16_t(cst)
-
+		// dataList 插入
 		dataList = append(dataList, digital)
 	}
-
-	close(ch)
 }
 
 // ReadStaticAnalogCsv 读取CSV文件, 将其转换成 []C.StaticAnalog 切片
-func ReadStaticAnalogCsv(filepath string) StaticAnalogSection {
-	// 打开文件
-	file, err := os.Open(filepath)
-	if err != nil {
-		panic("can not open file: " + filepath)
-	}
-	defer func() { _ = file.Close() }()
-
+func ReadStaticAnalogCsv(_ string) StaticAnalogSection {
 	return StaticAnalogSection{}
 }
 
 // ReadStaticDigitalCsv 读取CSV文件, 将其转换成 []C.StaticDigital 切片
-func ReadStaticDigitalCsv(filepath string) StaticDigitalSection {
-	// 打开文件
-	file, err := os.Open(filepath)
-	if err != nil {
-		panic("can not open file: " + filepath)
-	}
-	defer func() { _ = file.Close() }()
-
+func ReadStaticDigitalCsv(_ string) StaticDigitalSection {
 	return StaticDigitalSection{}
 }
 
 // FastWriteRealtimeSection 极速写入实时断面
-func FastWriteRealtimeSection(fastAnalogCh chan AnalogSection, fastDigitalCh chan DigitalSection, normalAnalogCh chan AnalogSection, normalDigitalCh chan DigitalSection, closeCh chan bool) {
-	closeSum := 0
+func FastWriteRealtimeSection(closeChan chan struct{}, fastAnalogCh chan AnalogSection, fastDigitalCh chan DigitalSection, normalAnalogCh chan AnalogSection, normalDigitalCh chan DigitalSection) {
+	closeNum := 0
 	for {
 		select {
-		case _ = <-fastAnalogCh:
-			// GlobalDylib.DyWriteAnalog(section)
-		case _ = <-fastDigitalCh:
-			// GlobalDylib.DyWriteDigital(section)
+		case section := <-fastAnalogCh:
+			GlobalDylib.DyWriteAnalog(section)
+		case section := <-fastDigitalCh:
+			GlobalDylib.DyWriteDigital(section)
 		case section := <-normalAnalogCh:
 			GlobalDylib.DyWriteAnalog(section)
-		case _ = <-normalDigitalCh:
-			// GlobalDylib.DyWriteDigital(section)
-		case _ = <-closeCh:
-			closeSum++
+		case section := <-normalDigitalCh:
+			GlobalDylib.DyWriteDigital(section)
+		case <-closeChan:
+			closeNum++
 		}
 
-		if closeSum == 2 {
+		if closeNum == 4 && len(fastAnalogCh) == 0 && len(fastDigitalCh) == 0 && len(normalAnalogCh) == 0 && len(normalDigitalCh) == 0 {
 			break
 		}
 	}
-	close(closeCh)
+	close(closeChan)
+	close(fastAnalogCh)
+	close(fastDigitalCh)
+	close(normalAnalogCh)
+	close(normalDigitalCh)
 }
 
 func WriteHistorySection() {
 }
 
 func FastWrite(fastAnalogCsvPath string, fastDigitalCsvPath string, normalAnalogCsvPath string, normalDigitalCsvPath string) {
+	closeCh := make(chan struct{}, 4)
 	fastAnalogCh := make(chan AnalogSection, CacheSize)
 	fastDigitalCh := make(chan DigitalSection, CacheSize)
-	close(fastAnalogCh)
-	close(fastDigitalCh)
 	normalAnalogCh := make(chan AnalogSection, CacheSize)
 	normalDigitalCh := make(chan DigitalSection, CacheSize)
-	closeCh := make(chan bool)
 	wg := new(sync.WaitGroup)
-
-	wg.Add(2)
-	// go ReadAnalogCsv(fastAnalogCsvPath, fastAnalogCh, wg, closeCh)
-	// go ReadDigitalCsv(fastDigitalCsvPath, fastDigitalCh, wg, closeCh)
-	go ReadAnalogCsv(normalAnalogCsvPath, normalAnalogCh, wg, closeCh)
-	go ReadDigitalCsv(normalDigitalCsvPath, normalDigitalCh, wg, closeCh)
-	FastWriteRealtimeSection(fastAnalogCh, fastDigitalCh, normalAnalogCh, normalDigitalCh, closeCh)
+	wg.Add(4)
+	go ReadAnalogCsv(wg, closeCh, fastAnalogCsvPath, fastAnalogCh)
+	go ReadDigitalCsv(wg, closeCh, fastDigitalCsvPath, fastDigitalCh)
+	go ReadAnalogCsv(wg, closeCh, normalAnalogCsvPath, normalAnalogCh)
+	go ReadDigitalCsv(wg, closeCh, normalDigitalCsvPath, normalDigitalCh)
+	FastWriteRealtimeSection(closeCh, fastAnalogCh, fastDigitalCh, normalAnalogCh, normalDigitalCh)
 	wg.Wait()
 }
 
@@ -400,37 +392,19 @@ func (df *DyLib) Logout() {
 	C.dy_logout(df.handle)
 }
 
-var AA = atomic.Int32{}
-
 func (df *DyLib) DyWriteAnalog(section AnalogSection) {
-	if len(section.Data) == 0 {
-		AA.Add(1)
-		return
-	}
 	C.dy_write_analog(df.handle, C.int64_t(section.Time), (*C.Analog)(&section.Data[0]), C.int64_t(len(section.Data)))
 }
 
 func (df *DyLib) DyWriteDigital(section DigitalSection) {
-	if len(section.Data) == 0 {
-		AA.Add(1)
-		return
-	}
 	C.dy_write_digital(df.handle, C.int64_t(section.Time), (*C.Digital)(&section.Data[0]), C.int64_t(len(section.Data)))
 }
 
 func (df *DyLib) DyWriteStaticAnalog(section StaticAnalogSection) {
-	if len(section.Data) == 0 {
-		AA.Add(1)
-		return
-	}
 	C.dy_write_static_analog(df.handle, (*C.StaticAnalog)(&section.Data[0]), C.int64_t(len(section.Data)))
 }
 
 func (df *DyLib) DyWriteStaticDigital(section StaticDigitalSection) {
-	if len(section.Data) == 0 {
-		AA.Add(1)
-		return
-	}
 	C.dy_write_static_digital(df.handle, (*C.StaticDigital)(&section.Data[0]), C.int64_t(len(section.Data)))
 }
 
@@ -438,6 +412,32 @@ var GlobalDylib *DyLib = nil
 
 func InitGlobalDylib(path string) {
 	GlobalDylib = NewDyLib(path)
+}
+
+// CrFilterReader 是一个自定义的 io.Reader，用于去除数据流中的 \r 字符
+type CrFilterReader struct {
+	reader *bufio.Reader
+}
+
+// NewCRFilterReader 返回一个包装了 bufio.Reader 的 crFilterReader
+func NewCRFilterReader(r *bufio.Reader) *CrFilterReader {
+	return &CrFilterReader{reader: r}
+}
+
+// Read 实现了 io.Reader 接口，去除读取数据流中的 \r 字符
+func (r *CrFilterReader) Read(p []byte) (int, error) {
+	n, err := r.reader.Read(p)
+	if err != nil {
+		return n, err
+	}
+
+	for i := 0; i < n; i++ {
+		if p[i] == '\r' {
+			p[i] = '\n'
+		}
+	}
+
+	return n, nil
 }
 
 func main() {
@@ -453,5 +453,4 @@ func main() {
 	normalAnalogCsvPath := wdDir + "/CSV20240614/1718350759143_REALTIME_NORMAL_ANALOG.csv"
 	normalDigitalCsvPath := wdDir + "/CSV20240614/1718350759143_REALTIME_NORMAL_DIGITAL.csv"
 	FastWrite(fastAnalogCsvPath, fastDigitalCsvPath, normalAnalogCsvPath, normalDigitalCsvPath)
-	fmt.Println(AA.Load())
 }
