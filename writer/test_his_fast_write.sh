344aca9dacf31c25d6dbe978c1047ae9fa373512
kkbase <kkbase@163.com>
2024-06-22 18:58:00 +0800

    删除writer

--- writer/test_his_fast_write.sh
+++
@@ -1,9 +0,0 @@
-./rtdb_writer static_write \
-    --plugin=../plugin_example/libcwrite_plugin.dylib \
-    --static_analog=../CSV20240614/1718350759143_HISTORY_NORMAL_STATIC_ANALOG.csv \
-    --static_digital=../CSV20240614/1718350759143_HISTORY_NORMAL_STATIC_DIGITAL.csv
-
-./rtdb_writer his_fast_write \
-    --plugin=../plugin_example/libcwrite_plugin.dylib \
-    --his_normal_analog=../CSV20240614/1718350759143_HISTORY_NORMAL_ANALOG.csv \
-    --his_normal_digital=../CSV20240614/1718350759143_HISTORY_NORMAL_DIGITAL.csv
