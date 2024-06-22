b60d6216befe96e0cf5845703dd115fffd932bb8
kkbase <kkbase@163.com>
2024-06-22 15:13:17 +0800

    add build.sh, test_his_fast_write.sh

---
+++ writer/build.sh
@@ -0,0 +1,1 @@
+go build -o rtdb_writer
---
+++ writer/test_his_fast_write.sh
@@ -0,0 +1,1 @@
+./rtdb_writer his_fast_write --plugin=../plugin_example/libcwrite_plugin.so --his_fast_analog=../CSV20240614/1718350759143_HISTORY_NORMAL_ANALOG.csv --his_fast_digital=../CSV20240614/1718350759143_HISTORY_NORMAL_DIGITAL.csv
