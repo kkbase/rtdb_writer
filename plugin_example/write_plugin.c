480d1c7196620e4a04171b92d1dc720f7a8f46f5
kkbase <kkbase@163.com>
2024-07-01 12:48:51 +0800

    up plugin example

--- plugin_example/write_plugin.c
+++ plugin_example/write_plugin.c
@@ -32,16 +32,20 @@ void write_rt_digital_list(int64_t unit_id, int64_t *time, Digital **digital_arr
 
 // 写历史模拟量
 void write_his_analog(int64_t unit_id, int64_t time, Analog *analog_array_ptr, int64_t count) {
+    printf("write his analog: time: %lld, count: %lld\n", time, count);
 }
 
 // 写历史数字量
 void write_his_digital(int64_t unit_id, int64_t time, Digital *digital_array_ptr, int64_t count) {
+    printf("write his digital: time: %lld, count: %lld\n", time, count);
 }
 
 // 写静态模拟量
 void write_static_analog(int64_t unit_id, StaticAnalog *static_analog_array_ptr, int64_t count) {
+    printf("write static analog: count: %lld\n", count);
 }
 
 // 写静态数字量
 void write_static_digital(int64_t unit_id, StaticDigital *static_digital_array_ptr, int64_t count) {
+    printf("write static digital: count: %lld\n", count);
 }
