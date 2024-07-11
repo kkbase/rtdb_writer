6d700f9e06fc44d76d3e70f35e11c63a80f4dfc1
kkbase <kkbase@163.com>
2024-07-11 16:07:44 +0800

    验证数据

--- plugin_example/write_plugin.c
+++ plugin_example/write_plugin.c
@@ -26,11 +26,28 @@ void write_rt_analog(int32_t magic, int64_t unit_id, int64_t time, Analog *analo
     }
     int sum = 0;
     if (time == 0) {
-        for (int i=0; i<count; i++) {
-            if (i == count-1) {
-                printf("%f\n", analog_array_ptr[i].av);
-            } else {
-                printf("%f ", analog_array_ptr[i].av);
+        // 验证随机av值
+        // for (int i=0; i<count; i++) {
+        //     if (i == count-1) {
+        //         printf("%f\n", analog_array_ptr[i].av);
+        //     } else {
+        //         printf("%f ", analog_array_ptr[i].av);
+        //     }
+        // }
+
+        // 验证GlobalID
+        for (int i = 0; i<count; i++) {
+            int64_t id = analog_array_ptr[i].global_id;
+            int64_t magic = id >> 32;
+            int64_t unit_id = (id & 0xFFFFFFFF) >> 24;
+            int64_t is_analog = (id & 0xFFFFFF) >> 23;
+            int64_t is_fast = (id & 0x7FFFFF) >> 22;
+            int64_t is_rt = (id & 0x3FFFFF) >> 21;
+            int64_t p_num = id & 0x1FFFFF;
+            printf("magic: %lld, unit_id: %lld, is_analog: %lld, is_fast: %lld, is_rt: %lld, p_num: %lld\n",
+                magic, unit_id, is_analog, is_fast, is_rt, p_num);
+            if (i > 3) {
+                break;
             }
         }
     }
@@ -45,6 +62,24 @@ void write_rt_digital(int32_t magic, int64_t unit_id, int64_t time, Digital *dig
         printf("write rt digital(fast): unit_id: %lld, time: %lld, count: %lld\n", unit_id, time, count);
     } else {
         printf("write rt digital(normal): unit_id: %lld, time: %lld, count: %lld\n", unit_id, time, count);
+    }
+
+    // 验证GlobalID
+    if (time == 0) {
+        for (int i = 0; i<count; i++) {
+            int64_t id = digital_array_ptr[i].global_id;
+            int64_t magic = id >> 32;
+            int64_t unit_id = (id & 0xFFFFFFFF) >> 24;
+            int64_t is_analog = (id & 0xFFFFFF) >> 23;
+            int64_t is_fast = (id & 0x7FFFFF) >> 22;
+            int64_t is_rt = (id & 0x3FFFFF) >> 21;
+            int64_t p_num = id & 0x1FFFFF;
+            printf("magic: %lld, unit_id: %lld, is_analog: %lld, is_fast: %lld, is_rt: %lld, p_num: %lld\n",
+                magic, unit_id, is_analog, is_fast, is_rt, p_num);
+            if (i > 3) {
+                break;
+            }
+        }
     }
 }
 
@@ -62,6 +97,24 @@ void write_rt_digital_list(int32_t magic, int64_t unit_id, int64_t *time, Digita
 void write_his_analog(int32_t magic, int64_t unit_id, int64_t time, Analog *analog_array_ptr, int64_t count) {
     printf("write his analog: unit_id: %lld, time: %lld, count: %lld\n", unit_id, time, count);
     int sum = 0;
+
+    if (time == 0) {
+        for (int i = 0; i<count; i++) {
+            int64_t id = analog_array_ptr[i].global_id;
+            int64_t magic = id >> 32;
+            int64_t unit_id = (id & 0xFFFFFFFF) >> 24;
+            int64_t is_analog = (id & 0xFFFFFF) >> 23;
+            int64_t is_fast = (id & 0x7FFFFF) >> 22;
+            int64_t is_rt = (id & 0x3FFFFF) >> 21;
+            int64_t p_num = id & 0x1FFFFF;
+            printf("magic: %lld, unit_id: %lld, is_analog: %lld, is_fast: %lld, is_rt: %lld, p_num: %lld\n",
+                magic, unit_id, is_analog, is_fast, is_rt, p_num);
+            if (i > 3) {
+                break;
+            }
+        }
+    }
+
     for (int i=0; i<10000000; i++) {
         sum++;
     }
